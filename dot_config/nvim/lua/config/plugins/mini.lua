3e867796ea75bf803d437c3be8b35105ade33ccd
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-23 15:58:39 +0100

    nvim: enable more mini plugins

--- dot_config/nvim/lua/config/plugins/mini.lua
+++ dot_config/nvim/lua/config/plugins/mini.lua
@@ -2,11 +2,15 @@ return {
   {
     'echasnovski/mini.nvim',
     config = function()
-      require('mini.statusline').setup { use_icons = true }
-      require('mini.jump').setup {}
       require('mini.ai').setup {}
-      require('mini.surround').setup {}
+      require('mini.cursorword').setup {}
+      require('mini.indentscope').setup {}
+      require('mini.jump').setup {}
       require('mini.pairs').setup {}
+      require('mini.statusline').setup { use_icons = true }
+      require('mini.surround').setup {}
+      require('mini.tabline').setup {}
+      require('mini.trailspace').setup {}
     end
   }
 }

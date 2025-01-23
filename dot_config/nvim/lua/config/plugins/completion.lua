9e446086a0c1663a857428752d46c7f5880641a2
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-23 16:52:37 +0100

    nvim: more autocompletion settings

--- dot_config/nvim/lua/config/plugins/completion.lua
+++ dot_config/nvim/lua/config/plugins/completion.lua
@@ -1,21 +1,22 @@
 return {
   {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'onsails/lspkind.nvim',
+    },
 
-    -- use a release tag to download pre-built binaries
     version = '*',
+
+    --- Option ideas
+    -- https://www.reddit.com/r/neovim/comments/1hlnv7x/blinkcmp_i_finally_have_a_configuration_that/
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = {
         preset = 'default',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
         ['<Tab>'] = {
           function(cmp)
             if cmp.snippet_active() then
@@ -32,22 +33,89 @@ return {
       },
 
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
         use_nvim_cmp_as_default = false,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'normal'
       },
 
-      -- Default list of enabled providers defined so that you can extend it
-
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+        providers = {
+          lsp = {
+            min_keyword_length = 2, -- Number of characters to trigger porvider
+            score_offset = 0,       -- Boost/penalize the score of the items
+          },
+          path = {
+            min_keyword_length = 0,
+          },
+          snippets = {
+            min_keyword_length = 2,
+          },
+          buffer = {
+            min_keyword_length = 4,
+            max_items = 4,
+          },
+        },
       },
-      signature = { enabled = true },
+
+      signature = {
+        enabled = true,
+        window = { border = "rounded" },
+      },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 250,
+          treesitter_highlighting = true,
+          window = { border = "rounded" },
+        },
+        menu = {
+          border = "rounded",
+
+          cmdline_position = function()
+            if vim.g.ui_cmdline_pos ~= nil then
+              local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+              return { pos[1] - 1, pos[2] }
+            end
+            local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+            return { vim.o.lines - height, 0 }
+          end,
+
+          draw = {
+            columns = {
+              { "kind_icon", "label", gap = 1 },
+              { "kind" },
+            },
+            components = {
+              kind_icon = {
+                text = function(item)
+                  local kind = require("lspkind").symbol_map[item.kind] or ""
+                  return kind .. " "
+                end,
+                highlight = "CmpItemKind",
+              },
+              label = {
+                text = function(item) return item.label end,
+                highlight = "CmpItemAbbr",
+              },
+              kind = {
+                text = function(item) return item.kind end,
+                highlight = "CmpItemKind",
+              },
+            },
+          },
+        }
+      }
     },
     opts_extend = { "sources.default" }
   }

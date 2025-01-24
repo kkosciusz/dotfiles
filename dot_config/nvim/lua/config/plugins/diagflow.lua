e36f42cd293617522e92954dd3bce80d1a5cdae2
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-24 15:03:54 +0100

    nvim: diagflow inline settings

--- dot_config/nvim/lua/config/plugins/diagflow.lua
+++ dot_config/nvim/lua/config/plugins/diagflow.lua
@@ -3,8 +3,15 @@ return {
     'dgagn/diagflow.nvim',
     opts = {
       scope = 'line',
+      text_align = 'left',
       placement = 'inline',
-      inline_padding_left = 0,
+      inline_padding_left = 2,
+      severity_colors = {
+        error = "DiagnosticVirtualTextError",
+        warn = "DiagnosticVirtualTextWarn",
+        info = "DiagnosticVirtualTextInfo",
+        hint = "DiagnosticVirtualTextHint",
+      },
     },
   }
 }

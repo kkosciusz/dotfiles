01769ca7120531326a91e88f42b6b98cd95b9126
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-24 23:26:42 +0100

    nvim: rewrite arista.lua

--- dot_config/nvim/lua/config/arista.lua
+++ dot_config/nvim/lua/config/arista.lua
@@ -1,5 +1,14 @@
 -- Include Arista specific settings
 local path = "/usr/share/vim/vimfiles/arista.vim"
 if vim.fn.filereadable(path) ~= 0 then
-  vim.cmd.source(path)
+  -- This botches all autocommands, disable for now
+  -- vim.cmd.source(path)
+  -- Selected entries included
+  vim.cmd([[syn match bugNumber "\c\<BUG\d\+"]])
+  vim.cmd([[hi def link bugNumber Todo]])
+  vim.g.python_recommended_style = 0
+  vim.opt.shiftwidth = 3
+  vim.opt.expandtab = true
+  table.insert(vim.opt.runtimepath, 0, "/usr/share/vim/vimfiles")
+  table.insert(vim.opt.runtimepath, "/usr/share/vim/vimfiles/after")
 end

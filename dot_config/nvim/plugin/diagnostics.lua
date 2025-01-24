31884f84fcfc61cbabc56fdd9f1600f3f22313e1
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-24 13:23:20 +0100

    nvim: configure diagnostic signs

---
+++ dot_config/nvim/plugin/diagnostics.lua
@@ -0,0 +1,37 @@
+vim.diagnostic.config {
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+    }
+  },
+}
+
+-- From help: diagnostic-handlers-example
+local ns = vim.api.nvim_create_namespace("my_namespace")
+local orig_signs_handler = vim.diagnostic.handlers.signs
+vim.diagnostic.handlers.signs = {
+  show = function(_, bufnr, _, opts)
+    local diagnostics = vim.diagnostic.get(bufnr)
+
+    local max_severity_per_line = {}
+    for _, d in pairs(diagnostics) do
+      local m = max_severity_per_line[d.lnum]
+      if not m or d.severity < m.severity then
+        max_severity_per_line[d.lnum] = d
+      end
+    end
+
+    local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+    orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+  end,
+  hide = function(_, bufnr)
+    orig_signs_handler.hide(ns, bufnr)
+  end,
+}

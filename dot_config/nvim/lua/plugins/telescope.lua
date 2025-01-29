f5abce6be6f171c26049f29986918a8859ae3abf
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-01-29 20:01:19 +0100

    nvim: add multigrep for selected text

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -47,6 +47,20 @@ return {
         require('telescope.builtin').find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }
       end)
       vim.keymap.set('n', '<space>fg', require("config.telescope").live_multigrep)
+      vim.keymap.set('x', '<space>fg', function()
+        vim.cmd.normal("") -- need to exit visual mode to update the marks
+        local start_pos = vim.fn.getcharpos("'<")
+        local end_pos = vim.fn.getcharpos("'>")
+        local text = vim.api.nvim_buf_get_text(
+          0,
+          start_pos[2] - 1,
+          start_pos[3] - 1,
+          end_pos[2] - 1,
+          end_pos[3],
+          {}
+        )[1] -- first line only
+        require("config.telescope").live_multigrep { default_text = text }
+      end)
       vim.keymap.set('n', '<space>ec', function()
         require('telescope.builtin').find_files {
           cwd = vim.fs.joinpath("~", ".config"),

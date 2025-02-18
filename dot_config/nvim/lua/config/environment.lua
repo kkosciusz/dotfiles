6f89b5499e06e7483c44f31e316fddaa7769b1de
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-02-18 11:17:35 +0100

    nvim: fix test for ms windows

--- dot_config/nvim/lua/config/environment.lua
+++ dot_config/nvim/lua/config/environment.lua
@@ -1,4 +1,4 @@
 -- figure out environment in which we are running
 Env = {}
 Env.is_connected_via_mosh = os.getenv("NCURSES_NO_UTF8_ACS") == "1"
-Env.is_os_windows = vim.fn.has('win32')
+Env.is_os_windows = vim.fn.has('win32') ~= 0

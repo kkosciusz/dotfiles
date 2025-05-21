625d6a6c829387d5beb8c2b1d46a9905b652bf49
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2025-05-21 14:15:39 +0200

    nvim: Set autowrite and autoread globally

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 
--- display
+-- display and layout
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 5
@@ -11,6 +11,9 @@ vim.o.cursorline = true
 vim.o.conceallevel = 1
 vim.o.listchars = "nbsp:⎵,tab:▸·"
 vim.o.list = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait100-blinkoff250-blinkon250"
 
 -- cursorline only in one buffer, disable in insert mode
 vim.api.nvim_create_autocmd({ 'InsertLeave', 'InsertEnter', 'WinEnter', 'WinLeave' }, {
@@ -21,17 +24,14 @@ vim.api.nvim_create_autocmd({ 'InsertLeave', 'InsertEnter', 'WinEnter', 'WinLeav
   end,
 })
 
+-- buffer behaviour
+vim.o.autoread = true
+vim.o.autowrite = true
+
 -- search
 vim.o.wrapscan = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- windows
-vim.o.splitbelow = true
-vim.o.splitright = true
-
--- cursor
-vim.o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait100-blinkoff250-blinkon250"
 
 -- os-specific
 if Env.is_os_windows then
